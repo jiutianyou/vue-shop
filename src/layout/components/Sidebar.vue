6dd59a0bffcdf5229cbcd6411cb959f3a6923afa
jiutianyou <46801386+jiutianyou@users.noreply.github.com>
2020-12-31 14:25:27 +0800

    upate

--- src/layout/components/Sidebar.vue
+++ src/layout/components/Sidebar.vue
@@ -13,59 +13,46 @@
     style="border-right: 0px;"
     ref="menu"
     :collapse="isCollapse">
-    <!-- item.type指菜单类型  0或者空是菜单展示，1是页面内标签不展示仅用作权限命中提供菜单id，其它值未定义 -->
     <template v-for="item in router" >
-      <el-submenu :key="item.id" v-if="item.nodeList !== undefined && !item.type" :index="item.url" >
+      <el-submenu :key="item.id" v-if="item.childList !== undefined && !item.type" :index="item.pathUrl" >
         <template slot="title">
           <i :class="item.icon" class="iconfont" style="margin-right: 10px"></i>
-          <span slot="title">{{item.displayText}}</span>
+          <span slot="title">{{item.label}}</span>
         </template>
-        <template v-if="item.nodeList !== undefined">
-          <el-menu-item :index="children.url" v-for="children in item.nodeList" :key="children.id">
+        <template v-if="item.childList !== undefined">
+          <el-menu-item :index="children.pathUrl" v-for="children in item.childList" :key="children.id">
             <i :class="children.icon"></i>
-            <span slot="title">{{children.displayText}}</span>
+            <span slot="title">{{children.label}}</span>
           </el-menu-item>
         </template>
       </el-submenu>
-      <el-menu-item :key="item.id" v-else-if="!item.type" :index="item.url" class="singer-menu" style="padding: 0 30px">
+      <el-menu-item :key="item.id" v-else-if="!item.type" :index="item.pathUrl" class="singer-menu" style="padding: 0 30px">
         <i :class="item.icon" class="iconfont" style="margin-right: 10px"></i>
-        <span slot="title">{{item.displayText}}</span>
+        <span slot="title">{{item.label}}</span>
       </el-menu-item>
     </template>
   </el-menu>
 </template>
 <script>
 import { menus } from '../../datas/menu.js'
-// import { mapState, mapActions } from 'vuex'
 export default {
   data () {
     return {
       activeIndex: null,
       activeOpeneds: null,
-      isCollapse: true,
+      isCollapse: false,
       router: []
     }
   },
   computed: {
-    // ...mapState({
-    //   isCollapse: state => state.Login.collapseStatus,
-    //   temRoutePath: state => state.Login.temRoutePath,
-    //   allUrl: state => Object.keys(state.Menu.allRouter || {})
-    // })
   },
   mounted () {
     this.activeIndex = this.$route.path
-    // await this.getMenuByUser()
-    // this.setActiveRouter(this.$route.path)
     console.log(menus)
     this.router = [...menus.data]
   },
   methods: {
-    // ...mapActions([
-    //   'setTemRoutePath',
-    //   'getMenuByUser',
-    //   'setActiveRouter'
-    // ]),
+
     handleOpen (key, keyPath) {
       console.log(key, keyPath)
     },
@@ -74,18 +61,11 @@ export default {
     },
     handleSelect (key, keyPath) {
       console.log(key, keyPath)
-      // this.setActiveRouter(key)
-      // this.setTemRoutePath(key)
     }
   },
   watch: {
     '$route' (v) {
       console.log(v)
-      // if (this.$refs.menu && this.allUrl.includes(v.path)) {
-      //   this.$refs.menu.updateActiveIndex(v.path)
-      // } else {
-      //   this.activeIndex = v.path
-      // }
     }
   }
 }
