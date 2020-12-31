9d9a9eada0975de438f210a6e42956acbe66268b
jiutianyou <46801386+jiutianyou@users.noreply.github.com>
2020-12-31 16:47:14 +0800

    update

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -3,7 +3,7 @@
     <br>
     <h3>项目介绍</h3>
     <ul>
-      <li>k-admin权限管理系统，是基于nodejs打造出的一站式RBAC管理平台</li>
+      <li>基于nest、vue、管理平台</li>
       <li>
         前端使用&nbsp;
         <a style="color: #0366d6;" target="_blank" href="https://github.com/PanJiaChen/vue-element-admin">vue-element-admin</a>
@@ -11,8 +11,7 @@
         <a style="color: #0366d6;" target="_blank" href="https://nestjs.com/">nestjs</a>
         &nbsp;<a style="color: #0366d6;" target="_blank" href="https://typeorm.io/">typeorm</a>&nbsp;mysql 等开发
       </li>
-      <li>优秀的菜单功能权限，前端可灵活控制页面及按钮展示，后端可对未授权的请求进行拦截</li>
-      <li>当前已有功能：用户管理，角色管理，部门管理，菜单管理，文件管理</li>
+      <li>一步一步学习</li>
     </ul>
     <br><br>
     <h3>下一步计划</h3>
